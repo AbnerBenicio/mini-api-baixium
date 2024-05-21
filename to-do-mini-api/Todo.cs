6dac88bf94f19bcca92ca7f004931e381fc40563
Abner Benicio <abnerbeniciosilva123@gmail.com>
2024-05-21 15:56:22 -0300

    Removendo classes

--- to-do-mini-api/Todo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace to_do_mini_api
-{
-    public class Todo
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
-}
