5b914577bbad6168d09e0d933654dfef26d59fe5
Abner Benicio <112874576+abnerbenicio@users.noreply.github.com>
2024-06-12 12:18:00 -0300

    Update Usuario.cs

--- to-do-mini-api/Model/Usuario.cs
+++ to-do-mini-api/Model/Usuario.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-
-namespace to_do_mini_api.Model
+﻿namespace to_do_mini_api.Model
 {
-    public class Usuario : IdentityUser<Guid>
+    public class Usuario
     {
+        public Guid Id { get; set; }
         public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
         public bool Administrador { get; set; }
+
     }
 }
 
