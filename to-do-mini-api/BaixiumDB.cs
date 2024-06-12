d3056e8de955b7275a26ec27fda0a9f781cc11c6
Abner Benicio <112874576+abnerbenicio@users.noreply.github.com>
2024-06-12 12:16:01 -0300

    Update BaixiumDB.cs

--- to-do-mini-api/BaixiumDB.cs
+++ to-do-mini-api/BaixiumDB.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using to_do_mini_api.Model;
 
 namespace to_do_mini_api;
-public class BaixumDB : IdentityDbContext<Usuario, IdentityRole<Guid>, Guid>
+public class BaixumDB : DbContext
 {
     public BaixumDB(DbContextOptions<BaixumDB> options)
         : base(options)
@@ -14,4 +12,3 @@ public class BaixumDB : IdentityDbContext<Usuario, IdentityRole<Guid>, Guid>
     public DbSet<Artigo> Artigos => Set<Artigo>();
     public DbSet<Tema> Temas => Set<Tema>();
 }
-
