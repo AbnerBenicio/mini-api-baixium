700b149b37bd16b3fae74a0564fe26a64e25c843
ArthurCremasco <arthurcremascoamaral7@gmail.com>
2024-06-12 09:06:40 -0300

    feat: AplTema

---
+++ to-do-mini-api/Services/AplTemas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using to_do_mini_api.Model;
+
+namespace to_do_mini_api.Services
+{
+    public class AplTemas
+    {
+        public async Task<Tema> PublicarTema(Tema tema, BaixumDB db)
+        {
+            if (tema != null)
+            {
+                db.Temas.Add(tema);
+                await db.SaveChangesAsync();
+                return tema;
+            }
+            else
+            {
+                throw new ArgumentException("Tema não existe");
+            }
+        }
+
+        public async Task<Tema> BuscarTema(BaixumDB db)
+        {
+             return await db.Temas.ToListAsync();
+        }
+    }
+}
